a79f417e28c4167e52818598632cf9cf381d33e9
Fabrício Ferreira <fabricio@fabriciofs.eti.br>
2020-06-17 08:10:09 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect}  from "react";
+
+import api from './services/api'
+
+import "./styles.css";
+
+function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  },[])
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      url: "https://github.com/Rocketseat/umbriel",
+      title: `Teste ${Date.now()}`,
+      techs: ["Node", "Express", "TypeScript"]
+    })
+    setRepositories([...repositories, response.data])
+  }
+
+  async function handleRemoveRepository(id) {
+    const indexRepository = repositories.findIndex(repository => repository.id === id)
+    
+    if (indexRepository < 0) {
+      alert(`ID ${id} inválido!`)
+      return
+    }
+
+    await api.delete(`repositories/${id}`)
+    repositories.splice(indexRepository, 1)
+    setRepositories([...repositories])
+
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {
+          repositories.map(repository => (
+            <li key={repository.id}>
+            {repository.title}
+  
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+          ))
+        }
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
